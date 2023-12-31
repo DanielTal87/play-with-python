3621e89a0db2a4c8f05f2c92b4e0e9756b1118c7
Daniel Tal <87daniel.tal@gmail.com>
2023-12-31 14:49:27 +0200

    add test

---
+++ test.py
@@ -0,0 +1,18 @@
+def add_numbers(a, b):
+    return a + b
+
+def multiply_numbers(a, b):
+    return a * b
+
+def greet(name):
+    return f"Hello, {name}!"
+
+# Testing the functions
+result_sum = add_numbers(5, 3)
+result_product = multiply_numbers(4, 7)
+greeting = greet("John")
+
+# Displaying the results
+print(f"Sum: {result_sum}")
+print(f"Product: {result_product}")
+print(greeting)
