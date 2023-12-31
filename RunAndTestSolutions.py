ab7fa83f579e7212fdf5f38cf305a4be1854a842
Daniel Tal <87daniel.tal@gmail.com>
2023-12-31 16:24:24 +0200

    LeetCode: 380

--- RunAndTestSolutions.py
+++ RunAndTestSolutions.py
@@ -1,18 +1,37 @@
 from typing import Optional
 
-from removeDuplicatesK import Solution1029, test_cases_1029
+from randomizedSet import RandomizedSet, test_cases_380
+from removeDuplicatesK import RemoveDuplicatesK, test_cases_1029
 
 
 def run_tests(tests):
     for solution_class, test_cases in tests:
         print(f"\033[33m\nRunning tests for {solution_class.__name__}:\033[0m")
 
-        if solution_class is Solution1029:
+        if solution_class is RemoveDuplicatesK:
             for index, test_case in enumerate(test_cases, start=1):
                 test_input, k, expected_output = test_case
                 solution = solution_class()
                 output = solution.removeDuplicates(test_input, k)
                 compare_expected_and_output(expected_output, output, index)
+
+        if solution_class is RandomizedSet:
+            randomized_set = solution_class()
+            outputs = []
+            for index, test_case in enumerate(test_cases, start=1):
+                operation, value, expected = test_case
+
+                if operation == "insert":
+                    result = randomized_set.insert(value)
+                elif operation == "remove":
+                    result = randomized_set.remove(value)
+                elif operation == "getRandom":
+                    result = randomized_set.getRandom()
+
+                outputs.append((result, expected))
+
+            for index, (output, expected) in enumerate(outputs, start=1):
+                compare_expected_and_output(expected, output, index)
 
 
 ##### Analyze test results functions #####
@@ -32,7 +51,7 @@ def compare_expected_and_output(expected, output, index: Optional[int] = None):
 
 run_tests(
     [
-        (Solution1029, test_cases_1029),
-        (Solution1029, test_cases_1029),
+        (RemoveDuplicatesK, test_cases_1029),
+        (RandomizedSet, test_cases_380),
     ]
 )
---
+++ randomizedSet.py
@@ -0,0 +1,52 @@
+# LeetCode: 380
+
+import random
+
+
+class RandomizedSet:
+    def __init__(self):
+        self.arr = []
+        self.dict = {}
+
+    def insert(self, val: int) -> bool:
+        if val in self.dict:
+            return False
+
+        index = len(self.arr)
+        self.arr.append(val)
+        self.dict[val] = index
+
+        return True
+
+    def remove(self, val: int) -> bool:
+        index = self.dict.get(val, None)
+        if index is None:
+            return False
+
+        length = len(self.arr)
+        last = self.arr[length - 1]
+        self.arr[index], self.arr[length - 1] = self.arr[length - 1], self.arr[index]
+
+        del self.arr[-1]
+        del self.dict[val]
+
+        if last != val:
+            self.dict[last] = index
+
+        return True
+
+    def getRandom(self) -> int:
+        index = random.randrange(0, len(self.arr))
+
+        return self.arr[index]
+
+
+test_cases_380 = [
+    ("insert", 1, True),
+    ("remove", 2, False),
+    ("insert", 2, True),
+    ("getRandom", None, 2),
+    ("remove", 1, True),
+    ("insert", 2, False),
+    ("getRandom", None, 2),
+]
